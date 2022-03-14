95387d53cf526d7912960252ed55ad70fc699c27
jinwq <jinwenqiu@foxmail.com>
2022-03-14 23:54:23 +0800

    fix:修复全局context

--- src/Component/Context/ContextChild.tsx
+++ src/Component/Context/ContextChild.tsx
@@ -7,7 +7,7 @@ interface PropsParam {
   count: number;
 }
 const ContextChild: React.FC<PropsParam> = ({ step, number, count }) => {
-  console.log(useContext(APPContext));
+  console.log('APPContext', useContext(APPContext));
   const { setStep, setNumber, setCount, fetchData } = useContext(APPContext);
   useEffect(() => {
     fetchData().then((res: any) => {
