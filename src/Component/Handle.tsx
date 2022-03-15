2358deb31970d9020d459233ae2c7a524334c8db
jinwq <jinwenqiu@foxmail.com>
2022-03-16 00:04:12 +0800

    fix:窗口改变状态值响应

--- src/Component/Handle.tsx
+++ src/Component/Handle.tsx
@@ -4,6 +4,7 @@ const Handle: React.FC = () => {
   useEffect(() => {
     const handle = () => {
       document.title = Math.random().toString();
+      console.log('render');
     };
     window.addEventListener('resize', handle);
     return () => {
