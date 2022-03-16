d10dd15b4b98a23896c8c5683e82eb63b9613889
jinwq <jinwenqiu@foxmail.com>
2022-03-17 00:06:47 +0800

    style:样式修改

--- src/Component/Article.tsx
+++ src/Component/Article.tsx
@@ -1,26 +1,28 @@
-import  React, { useState }  from 'react';
+import React, { useState } from 'react';
 
 type ArticleInfo = {
-  title:string,
-  content:string
-}
+  title: string;
+  content: string;
+};
 
-const Article:React.FC<ArticleInfo> = ({title,content}) =>{
-  const [article,setArticle] = useState<ArticleInfo>({title,content});
-  return(
+const Article: React.FC<ArticleInfo> = ({ title, content }) => {
+  const [article, setArticle] = useState<ArticleInfo>({ title, content });
+  return (
     <>
-    <div>Title:{article.title}</div>
-    <section>{article.content}</section>
-    <button onClick={()=>setArticle({
-      title:'下一篇',
-      content:'下一篇到内容',
-    })}>
-      下一篇
-    </button>
+      <div>Title:{article.title}</div>
+      <section>{article.content}</section>
+      <button
+        onClick={() =>
+          setArticle({
+            title: '下一篇',
+            content: '下一篇到内容',
+          })
+        }
+      >
+        下一篇
+      </button>
     </>
-  )
-}
+  );
+};
 
-export default Article
-
-
+export default Article;
